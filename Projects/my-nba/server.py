18c4daa36f8b7aef02d912bcf92b18d0b20f3b37
elikkozak <elik.kozak@gmail.com>
2022-09-20 15:34:02 +0300

    added func to get player pos and jersey

--- Projects/my-nba/server.py
+++ Projects/my-nba/server.py
@@ -19,11 +19,19 @@ def init():
 def get_player_name(player_data):
     return player_data["firstName"] + " " + player_data["lastName"]
 
+def get_player_pos(player_data):
+    return player_data.get("pos")
+    
+def get_player_jersey_num(player_data):
+    return player_data.get("jersey")
+
 
 @app.get("/{team}/{year}")
 def get_player_data(team,year):
     player_data_dict = requests.get(f'http://data.nba.net/10s/prod/v1/{year}/players.json')
-    return {get_player_name(player_data) for player_data in player_data_dict.json()["league"]["standard"]}
+    print(len(player_data_dict.json()["league"]["standard"]))
+    # return [get_player_pos(player_data) for player_data in player_data_dict.json()["league"]["standard"]]
+    return player_data_dict.json()["league"]["standard"]
 
 
 if __name__ == "__main__":
