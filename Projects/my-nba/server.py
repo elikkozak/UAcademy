93c1786cf324f5cd2f8866a4078a20d4864dd70a
elikkozak <elik.kozak@gmail.com>
2022-09-30 19:14:01 +0300

    added static files

--- Projects/my-nba/server.py
+++ Projects/my-nba/server.py
@@ -4,8 +4,19 @@ import uvicorn
 from fastapi import FastAPI
 
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
+
+app.mount("/client/build", StaticFiles(directory="client/build"), name="static")
+
+@app.get('/')
+def root():
+    return FileResponse('./client/build/index.html')
+
+data_holder = []
+is_data_init = False
 
 team_to_ids = {
     "lakers": "1610612747",
@@ -13,9 +24,6 @@ team_to_ids = {
     "heat": "1610612748",
     "suns": "1610612756"
 }
-@app.get("/")
-def init():
-    return "Hello World"
 
 def get_player_name(player_data):
     return player_data["firstName"] + " " + player_data["lastName"]
@@ -41,10 +49,13 @@ def create_player_obj(player_data):
 @app.get("/players")
 def get_player_data(team,year):
     player_data_req = requests.get(f'http://data.nba.net/10s/prod/v1/{year}/players.json')
-    filtered_data_by_team = filter(lambda player_data: player_data["teamId"] == team_to_ids[team],player_data_req.json()["league"]["standard"])
-    players_data_list =  [create_player_obj(player_data) for player_data in filtered_data_by_team]
+    filtered_data = list(filter(lambda player_data: player_data["teamId"] == team_to_ids[team],player_data_req.json()["league"]["standard"]))
+    data_holder = filtered_data
+    is_data_init = True
+    players_data_list =  [create_player_obj(player_data) for player_data in filtered_data]
+   
     return players_data_list
-    
+
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
