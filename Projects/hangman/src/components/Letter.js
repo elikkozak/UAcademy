0239b17fc28d423d7bff986393f5b56ad1e8cffd
elikkozak <elik.kozak@gmail.com>
2022-11-04 20:25:37 +0200

    added component letter

---
+++ Projects/hangman/src/components/Letter.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Letter extends Component {
+  render() {
+    return (
+      <div>
+        a
+      </div>
+    );
+  }
+}
+
+export default Letter;
