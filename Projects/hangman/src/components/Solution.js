4ca5fa29d8b899f00af573ad914f9f310e0ef09b
elikkozak <elik.kozak@gmail.com>
2022-11-04 21:58:04 +0200

    displaying underscore or the letter based on status

--- Projects/hangman/src/components/Solution.js
+++ Projects/hangman/src/components/Solution.js
@@ -5,9 +5,9 @@ class Solution extends Component {
   render() {
     return (
       <div>
-        {
-        [...this.props.solution.word].map((letter,index) => <Letter key = {index} letter ={letter} />)
-        }
+        {[...this.props.solution.word]
+          .map((letter, index) => <Letter key={index} letter={letter} />)
+          .map((letter) => (this.props.letterStatus[letter] ? letter : " _ "))}
         <div>
           <em>{this.props.solution.hint}</em>
         </div>
