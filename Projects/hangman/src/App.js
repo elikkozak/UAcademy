55a7fee6b5f77590b2a416ac493376442da4e9ea
elikkozak <elik.kozak@gmail.com>
2022-11-04 21:25:09 +0200

    converted app to class and added letterStatus

--- Projects/hangman/src/App.js
+++ Projects/hangman/src/App.js
@@ -1,16 +1,37 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+
 import "./App.css";
 import Solution from "./components/Solution";
 import Letters from "./components/Letters";
 import Score from "./components/Score";
 
-function App() {
-  return (
-    <div className="App">
-      <Score /> <Solution />
-      <Letters />
-    </div>
-  );
+const A_ASCII_CODE = 65;
+const Z_ASCII_CODE = 90;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      letterStatus: this.generateLetterStatuses(),
+    };
+  }
+
+  generateLetterStatuses() {
+    let myObj = {};
+    for (let i = A_ASCII_CODE; i <= Z_ASCII_CODE; i++) {
+      myObj[String.fromCharCode(i)] = false;
+    }
+    return myObj
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Score /> <Solution />
+        <Letters />
+      </div>
+    );
+  }
 }
 
 export default App;
