70f6da5bae78680a80e619bf85614719c6ddcb90
elikkozak <elik.kozak@gmail.com>
2022-11-07 21:07:37 +0200

    created all the logic behind the check if game has finished

--- Projects/hangman/src/App.js
+++ Projects/hangman/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Solution from "./components/Solution";
 import Letters from "./components/Letters";
 import Score from "./components/Score";
+import EndGame from "./components/EndGame";
 
 const A_ASCII_CODE = 65;
 const Z_ASCII_CODE = 90;
@@ -18,26 +19,85 @@ class App extends Component {
         hint: "HINT",
       },
       score: 100,
+      gameFinished: false,
     };
   }
 
   generateLetterStatuses() {
-    let myObj = {};
+    let letterStatus = {};
     for (let i = A_ASCII_CODE; i <= Z_ASCII_CODE; i++) {
-      myObj[String.fromCharCode(i)] = false;
+      letterStatus[String.fromCharCode(i)] = false;
     }
-    return myObj;
+    return letterStatus;
   }
 
+  isGameWon = (letterStatus) => {
+    // [...this.state.solution.word].forEach((letter) => {
+    //   if (!this.state.letterStatus[letter]) {
+    //     return false;
+    //   }
+    // });
+
+    for (const letter of this.state.solution.word) {
+      if (!letterStatus[letter]){
+        return false
+      }
+  }
+
+    return true;
+  };
+
+  updateScore = (letterStatus,letter) => {
+    let newScore = 0;
+    if (this.state.solution.word.includes(letter)) {
+      newScore = this.state.score + 5;
+    } else {
+      newScore = this.state.score - 20;
+    }
+    this.setState({
+      score: newScore,
+    });
+
+    if (newScore <= 0 || this.isGameWon(letterStatus)) {
+      this.setState({
+        gameFinished: true,
+      });
+    }
+  };
+
+  selectLetter = (letter) => {
+    if (!this.state.letterStatus[letter]) {
+      let letterStatusNew = { ...this.state.letterStatus };
+      letterStatusNew[letter] = true;
+      this.setState(
+        {
+          letterStatus: letterStatusNew,
+        },
+        this.updateScore(letterStatusNew,letter)
+      );
+    }
+  };
+
   render() {
+    let gameFinished = this.state.gameFinished;
     return (
       <div className="App">
-        <Score score={this.state.score} />
-        <Solution
-          letterStatus={this.state.letterStatus}
-          solution={this.state.solution}
-        />
-        <Letters letterStatus={this.state.letterStatus} />
+        {gameFinished ? (
+          <EndGame gameWon={this.state.score > 0} />
+        ) : (
+          <div>
+            <Score score={this.state.score} />
+
+            <Solution
+              letterStatus={this.state.letterStatus}
+              solution={this.state.solution}
+            />
+            <Letters
+              selectLetter={this.selectLetter}
+              letterStatus={this.state.letterStatus}
+            />
+          </div>
+        )}
       </div>
     );
   }
