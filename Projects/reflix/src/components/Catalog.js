de2455f191ce6eb87cd03018acfdfdd013f8d7b5
elikkozak <elik.kozak@gmail.com>
2022-11-09 12:15:16 +0200

    created basic catalog and home components

---
+++ Projects/reflix/src/components/Catalog.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Catalog extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Catalog;
